66a3218f3df4d35d564ff1c3cad6b12b00455873
chanhuijo <whcksgml.git@gmail.com>
2023-11-06 14:31:22 +0900

    week11 v2.4

--- week11/main.go
+++ week11/main.go
@@ -9,9 +9,22 @@ func main() {
 	// primes[2] = 5
 	// fmt.Println(primes, primes[1])
 
-	var primes [3]int=[3]int{2,3,5}
-	fmt.Println(primes, primes[1])
+	var primes [3]int = [3]int{2, 3, 5} //단축연산자
+	// fmt.Println(primes[4]) //invalid argument: index 4 out of bounds [0:3]
 
-	test:=[5]bool{true,true,true}
-	fmt.Println(test[3])
+	// test:=[5]bool{true,true,true}
+	// fmt.Println(test[3]) //boolean타입의 제로값, false
+
+	i := 0
+	// for i<4 { //panic: runtime error: index out of range [3] with length 3
+	for i < len(primes) {
+		fmt.Println(primes[i])
+		i++
+	}
+
+	// for prime := range primes { //값만 출력하려 했으나 인덱스가 출력됨
+	for index, prime := range primes { //컴파일에러, idx 사용해야함
+	for _, prime := range primes {
+		fmt.Println(prime)
+	}
 }
