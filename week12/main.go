61ce01790dbe095ba529ab1e07af037594d44afd
chanhuijo <whcksgml.git@gmail.com>
2023-11-13 14:30:54 +0900

    week12 v2.7

--- week12/main.go
+++ week12/main.go
@@ -13,4 +13,12 @@ func main() {
 	for _, value := range s {
 		fmt.Println(value)
 	}
+	copyS:=s[1:4]
+	for _, value := range copyS {
+		fmt.Println(value)
+	}
+	test:=[3]string{"inha","go","student"} // 배열 리터럴을 이용해서 test 배열 생성
+	// testS:=test[0:4] // invalid argument: index 4 out of bounds [0:4]
+	testS:=test[0:2]
+	fmt.Println(len(testS))
 }
