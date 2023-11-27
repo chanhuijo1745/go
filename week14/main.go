338b4611e7640fd4c29d92c2f00cec018ee0d752
chanhuijo <whcksgml.git@gmail.com>
2023-11-27 14:37:05 +0900

    week14 v4.7

--- week14/main.go
+++ week14/main.go
@@ -5,28 +5,11 @@ import (
 )
 
 func main() {
-	// map literal
-	games:=map[int]string{
-		456:"성기훈",
-		218:"박해수",
-		067:"강새벽",
-		001:"오일남",
-		199:"알리",
-		101:"아이오아이",
-	}
-	//fmt.Println(games[100])
-	//name,ok:=games[100]
-	name,ok:=games[101]
-	fmt.Println(name,ok)
+	//var balls map[string]int
+	balls:=make(map[string]int)
+	fmt.Printf("%#v\n",balls)
 
-	for _,v:=range games {
-		fmt.Println(v)
-	}
-
-	games[101]="장덕수" // update
-	delete(games,199) // delete
-
-	for k,v:=range games {
-		fmt.Println(k,v)
-	}
+	balls["성기훈"]=20
+	fmt.Println(balls["성기훈"])
+	fmt.Println(balls["오일남"])
 }
